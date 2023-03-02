7faac47e551de40d53d08db7cd054a1053c0de8a
dethMastery <me@suphakit.net>
2023-03-03 00:24:04 +0700

    feat: set up

---
+++ font-lineseed-th.rb
@@ -0,0 +1,20 @@
+FAMILY: LINE Seed Sans TH
+cask 'FIXME' do
+  version "1.002"
+  sha256 "3a06a1549f7c2cb4f9c9b9c99afe3701f7f6aa47039e6073ab18336dbc582436"
+
+  url "https://seed.line.me/"
+  name "LINE Seed Sans TH"
+  homepage "https://seed.line.me/"
+
+  font "LINESeedSansTH_Bd.otf"
+  font "LINESeedSansTH_Bd.ttf"
+  font "LINESeedSansTH_He.otf"
+  font "LINESeedSansTH_He.ttf"
+  font "LINESeedSansTH_Rg.otf"
+  font "LINESeedSansTH_Rg.ttf"
+  font "LINESeedSansTH_Th.otf"
+  font "LINESeedSansTH_Th.ttf"
+  font "LINESeedSansTH_XBd.otf"
+  font "LINESeedSansTH_XBd.ttf"
+end
